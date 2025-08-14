0cbceee2a58c7ca759fafc7df8713f88f0d397ad
Jonas Svedstrup <jonassvedstrup@gmail.com>
2025-08-14 09:17:34 +0200

    Updates

--- assets/AboutView-DBNSri7O.js
+++
@@ -1,1 +0,0 @@
-import{d as o,k as n,c as a,a as t,o as s}from"./index-C2XIRoRm.js";const i={id:"main-about"},l=o({__name:"AboutView",setup(r){return n(),(u,e)=>(s(),a("main",i,e[0]||(e[0]=[t("section",null,[t("h1",null,"About")],-1)])))}});export{l as default};
